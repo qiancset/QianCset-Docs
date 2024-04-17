79dadaefc0452b68b316a9b6061100238f3865a4
qinjiangban <tanjiangban@gmail.com>
2024-04-18 04:34:28 +0800

    Initial commit

---
+++ docs/intro.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+---
+
+# 📖 概述
+
+      **文档为拟草案**，请以正式文档上线为准！
+      官网链接:https://www.qiancset.com
---
+++ docusaurus.config.ts
@@ -0,0 +1,129 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://docs.qiancset.com/',
+  baseUrl: '/',
+  organizationName: 'qiancset',
+  projectName: 'QianCset-Docs',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  
+ //即使不使用国际化，也可以使用此字段来设置有用的元数据，如 HTML lang。
+ //例如，如果您的网站是中文的，则您可能想用“zh-Hans”替换“en”。
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/favicon.ico',
+    navbar: {
+      title: 'QianCset',
+      logo: {
+        alt: 'Logo',
+        src: 'img/favicon.ico',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: '文档',
+        },
+        {to: '/blog', label: '博客', position: 'left'},
+        {
+          href: 'https://github.com/qiancset',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+
+/*     
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    }, */
+
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
